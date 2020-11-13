9626e4ddc8a9099d701a0c73a889317a37e13794
Daniel Omiya <danielomiya@gmail.com>
2020-11-13 00:44:32 -0300

    Add implementation of 'prog'

--- src/compaction.c
+++ src/compaction.c
@@ -1,9 +1,36 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "compaction/compaction.h"
 
+#define BUFFER_SIZE 1024
 #define STRING_EMPTY ""
+#define NUL '\0'
 
 const char *prog(const char *word) {
-    return STRING_EMPTY;
+    int i, count = 1, length = strlen(word);
+    char buffer[BUFFER_SIZE], *cursor = buffer;
+
+    for (i = 1; i <= length; i++) {
+        if (word[i] == word[i-1] && word[i] != NUL) {
+            count++;
+            continue;
+        }
+
+        if (buffer == cursor) cursor += sprintf(cursor, "%c%d", word[i-1], count);
+        else cursor += sprintf(cursor, "-%c%d", word[i-1], count);
+
+        count = 1; // reset count
+    }
+
+    length = strlen(buffer); // get length of used space on buffer
+
+    cursor = malloc(sizeof(char)*(length+1));
+    strcpy(cursor, buffer);
+    // cursor is now an array with only the number
+    // of positions necessary for our compacted string
+
+    return cursor;
 }
 
 const char *prog2(const char *word) {
--- tests/test_compaction.cpp
+++ tests/test_compaction.cpp
@@ -1,5 +1,27 @@
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 
-TEST_CASE("prog", "compacts repeated characters") {
+extern "C" {
+#include "compaction/compaction.h"
+#include "compaction/helpers.h"
+}
+
+TEST_CASE("compacts repeated characters", "[prog]") {
+
+    SECTION("simple compaction case") {
+        const char *input = "aaaaaaabbbbbaaaaaaaaaa";
+        const char *expected = "a7-b5-a10";
+        const char *result = prog(input);
+
+        REQUIRE(str_eq(result, expected));
+    }
+
+    SECTION("non repeated characters case") {
+        const char *input = "abcabc";
+        const char *expected = "a1-b1-c1-a1-b1-c1";
+        const char *result = prog(input);
+
+        REQUIRE(str_eq(result, expected));
+    }
+
 }
