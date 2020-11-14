dbd6355d28e6cf91c614ec4dd060c0f95240faa5
Daniel Omiya <danielomiya@gmail.com>
2020-11-14 00:22:29 -0300

    Add unit tests for 'desprog'

--- tests/test_compaction.cpp
+++ tests/test_compaction.cpp
@@ -25,3 +25,39 @@ TEST_CASE("compacts repeated characters", "[prog]") {
     }
 
 }
+
+TEST_CASE("uncompacts a string", "[desprog]") {
+
+    SECTION("groups of single char compacted") {
+        const char *input = "a7-b5-a10";
+        const char *expected = "aaaaaaabbbbbaaaaaaaaaa";
+        const char *result = desprog(input);
+
+        REQUIRE(str_eq(result, expected));
+    }
+
+    SECTION("single group of compacted chars") {
+        const char *input = "ab11";
+        const char *expected = "ababababababababababab";
+        const char *result = desprog(input);
+
+        REQUIRE(str_eq(result, expected));
+    }
+
+    SECTION("groups of single and multiple chars compacted") {
+        const char *input = "ab6-a15";
+        const char *expected = "ababababababaaaaaaaaaaaaaaa";
+        const char *result = desprog(input);
+
+        REQUIRE(str_eq(result, expected));
+    }
+
+    SECTION("non hyphenized groups") {
+        const char *input = "abc4xy3c7";
+        const char *expected = "abcabcabcabcxyxyxyccccccc";
+        const char *result = desprog(input);
+
+        REQUIRE(str_eq(result, expected));
+    }
+
+}
